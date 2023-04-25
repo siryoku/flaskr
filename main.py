c5db7cf8f0d1a90ca90b3d9668eebde7d3d8af4c
Shibata <suguru.shibata@blowish.jp>
2023-04-25 11:52:10 +0900

    initialcommit

---
+++ main.py
@@ -0,0 +1,44 @@
+from flaskr import app
+from flask import render_template, request, redirect, url_for
+import sqlite3
+DATABASE = "database.db"
+
+@app.route('/')
+def index():
+  con = sqlite3.connect(DATABASE)
+  db_books = con.execute("SELECT * FROM books").fetchall()
+  con.close()
+  # books = [
+  #   {"title": "はらぺこあおむし",
+  #    "price": 1200,
+  #    "arrival_day": "2020年7月12日"},
+  #   {"title": "ぐりとぐら",
+  #    "price": 990,
+  #    "arrival_day": "2020年7月13日"},
+  # ]
+  books=[]
+  for row in db_books:
+    books.append({"title": row[0], "price": row[1], "arrival_day": row[2]})
+  return render_template(
+    "index.html",
+    books=books
+  )
+
+@app.route('/form')
+def form():
+  return render_template(
+    "form.html"
+  )
+
+@app.route("/register", methods=["POST"])
+def register():
+  title = request.form["title"]
+  price = request.form["price"]
+  arrival_day = request.form["arrival_day"]
+
+  con = sqlite3.connect(DATABASE)
+  con.execute("INSERT INTO books VALUES(?,?,?)",
+  [title, price, arrival_day])
+  con.commit()
+  con.close()
+  return redirect(url_for("index"))
